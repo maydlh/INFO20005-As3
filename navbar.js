eb0141c9a305d75b4dbb609eb266a5068a9f8e17
May Ding <lehand@student.unimelb.edu.au>
2025-05-23 15:10:55 +1000

    add menu subitems

--- navbar.js
+++ navbar.js
@@ -6,4 +6,14 @@ const menuItem = document.querySelector(".menu-item");
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active");
     menuItem.classList.toggle("active");
+    document.body.classList.toggle("noscroll");
+})
+
+
+//menu in shop
+const shop = document.querySelector(".nav-item-shop");
+shop.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    menuItem.classList.toggle("active");
+    document.body.classList.toggle("noscroll");
 })
