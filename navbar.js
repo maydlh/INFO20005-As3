4b1fe8149661db99d1f31453a30237e11264fa85
May Ding <lehand@student.unimelb.edu.au>
2025-05-23 18:49:22 +1000

    add interactivity to laptop so the user can navigate to all watercolour

--- navbar.js
+++ navbar.js
@@ -11,11 +11,28 @@ hamburger.addEventListener("click",()=>{
 
 
 //menu in shop in laptop
+
+//all the clickable buttons
 const shop = document.querySelector(".nav-item-shop-laptop");
+const painting = document.querySelector(".nav-item-painting");
+const watercolour = document.querySelector(".nav-item-watercolour");
+
+
+//all the tabs that opens
 const shopItem = document.querySelector(".shop-expanded");
+const paintingItem = document.querySelector(".painting-expanded");
+const watercolourItem = document.querySelector(".watercolour-expanded");
+
 shop.addEventListener("click",()=>{
     shopItem.classList.toggle("active");
-})
+}) //display 1st list of subitem
 
-//laptop: menu option sfter shop is clicked
+painting.addEventListener("click",()=>{
+    paintingItem.classList.toggle("active");
+}) //display 2nd list of subitem
+
+watercolour.addEventListener("click",()=>{
+    watercolourItem.classList.toggle("active");
+}) //display 3rdd list of subitem
+
 
