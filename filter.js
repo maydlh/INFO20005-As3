c2e67355d71f8fadd98397be3ae333f517d99b9c
May Ding <lehand@student.unimelb.edu.au>
2025-05-24 14:17:54 +1000

    add filter function to type

--- filter.js
+++ filter.js
@@ -11,3 +11,32 @@ filterToggles.forEach(toggle => {
     icon.classList.toggle("active");  
   });
 });
+
+//click on daniel smith to remove all sennelier product
+
+const checkboxBrand = document.getElementById("daniel-smith");
+const sennelier = document.querySelectorAll("div.sen");
+
+checkboxBrand.addEventListener("change", function () {
+    sennelier.forEach(item => {
+      if (checkboxBrand.checked) {
+        item.style.display = "none";
+      } else {
+        item.style.display = "block"; // or whatever display they originally had
+      }
+    });
+  });
+
+
+  const checkboxType = document.getElementById("mg");
+const mediumAndGround = document.querySelectorAll("div.product-item:not(.mg)");
+
+checkboxType.addEventListener("change", function () {
+    mediumAndGround.forEach(item => {
+      if (checkboxType.checked) {
+        item.style.display = "none";
+      } else {
+        item.style.display = "block"; // or whatever display they originally had
+      }
+    });
+  });
